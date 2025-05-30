07dec59c58cd8d8b73d212b5bccfa73d18e6b37d
Luanipe <luanaraujo926@gmail.com>
2025-05-30 16:39:41 -0400

    feat: implement a new module to store custom exceptions

---
+++ backend/app/core/exceptions.py
@@ -0,0 +1,12 @@
+from fastapi import HTTPException, status
+from typing import Any, Optional, Dict
+
+
+class NotFoundError(HTTPException):
+    def __init__(self, detail: Any = None, headers: Optional[Dict[str, Any]] = None) -> None:
+        super().__init__(status.HTTP_404_NOT_FOUND, detail, headers)
+
+
+class DuplicatedError(HTTPException):
+    def __init__(self, detail: Any = None, headers: Optional[Dict[str, Any]] = None) -> None:
+        super().__init__(status.HTTP_400_BAD_REQUEST, detail, headers)
